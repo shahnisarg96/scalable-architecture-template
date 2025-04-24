e6be188ff5c452b1674c29b0158daf32b21637bd
Nisarg Shah <shahnisarg96@gmail.com>
2025-04-24 14:03:42 +0530

    Port changes for inter service calls

--- UMS-Enrollment-Service/src/main/java/com/ums/enrollment/constants/EndpointConstants.java
+++ UMS-Enrollment-Service/src/main/java/com/ums/enrollment/constants/EndpointConstants.java
@@ -1,7 +1,6 @@
 package com.ums.enrollment.constants;
 
 public class EndpointConstants {
-
-    public static final String GET_STUDENT_ENDPOINT="http://student-service:8080/students/";
-    public static final String GET_COURSE_ENDPOINT="http://course-service:8081/courses/";
+    public static final String GET_STUDENT_ENDPOINT="http://student-service:8082/students/";
+    public static final String GET_COURSE_ENDPOINT="http://course-service:8083/courses/";
 }
--- UMS-Faculty-Service/src/main/java/com/ums/faculty/constants/EndpointConstants.java
+++ UMS-Faculty-Service/src/main/java/com/ums/faculty/constants/EndpointConstants.java
@@ -1,6 +1,5 @@
 package com.ums.faculty.constants;
 
 public class EndpointConstants {
-
-    public static final String GET_COURSE_ENDPOINT="http://course-service:8081/courses/";
+    public static final String GET_COURSE_ENDPOINT="http://course-service:8083/courses/";
 }
